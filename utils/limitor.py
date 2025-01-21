637ffac7315293a19ba0d24afe28197a4a6b43bc
MasakiMu319 <bancangbaize@gmail.com>
2025-01-21 08:56:10 +0800

    ♻️ refactor(utils) : refact the design of limitor.

--- utils/limitor.py
+++ utils/limitor.py
@@ -9,33 +9,39 @@ LIMIT_REDIS_URI = f"{BASE_REDIS_URI}/{settings.redis.limitor.db}"
 
 
 class Limitor:
-    def __init__(self):
+    def __init__(self, key: str, period: int, max_count: int, timeout: float = 2.0):
         redis_pool = aioredis.BlockingConnectionPool.from_url(url=LIMIT_REDIS_URI)
         self.redis_client = aioredis.Redis.from_pool(connection_pool=redis_pool)
+        self.key = key
+        self.period = period
+        self.max_count = max_count
+        self.timeout = timeout
 
-    async def is_action_allowed_with_block(
-        self, key: str, period: int, max_count: int, timeout: float = 2.0
-    ) -> bool:
+    async def is_action_allowed_with_block(self) -> bool:
         """
         Judge if the action is allowd.
         """
         start_time = time.perf_counter()
         while True:
-            if time.perf_counter() - start_time > timeout:
+            if time.perf_counter() - start_time > self.timeout:
                 return False
 
             now = time.time()
-            window_start = now - period
-            key = f"ratelimit:{key}" if not key.startswith("ralimit:") else key
+            window_start = now - self.period
+            key = (
+                f"ratelimit:{self.key}"
+                if not self.key.startswith("ralimit:")
+                else self.key
+            )
             async with self.redis_client.pipeline() as pipe:
                 pipe.zremrangebyscore(key, 0, window_start)
                 pipe.zcount(key)
                 pipe.zadd(key, {f"{now:.9f}": now})
-                pipe.expire(key, period)
+                pipe.expire(key, self.period)
 
                 _, current_count, _, _ = await pipe.execute()
 
-                if current_count >= max_count:
+                if current_count >= self.max_count:
                     pipe.zrem(key, f"{now:.9f}")
                     await pipe.execute()
                     continue
